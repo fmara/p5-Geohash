d04d23410be2ba4490b84ff13fe3ed3e66ece390
Kazuhiro Osawa <yappo@shibuya.pl>
2012-09-30 15:18:34 +0900

    add split method

--- lib/GeoHash.pm
+++ lib/GeoHash.pm
@@ -108,6 +108,11 @@ sub merge {
     sort @results;
 }
 
+sub split {
+    my($self, $geohash) = @_;
+    map { "$geohash$_" } @ENC;
+}
+
 
 {
     package GeoHash::backendPP;
@@ -287,7 +292,17 @@ Merged with the larger area from geohash list. And remove duplicated geohash in 
     /);
     is($list[0], 'c2b25ps');
 
-=head2 @list_of_geohashes = $gh->crash(@list_of_merged_geohashes)
+=head2 @list_of_geohashes = $gh->split(@list_of_merged_geohashes)
+
+geohash splitter.
+
+    my @list = $gh->split('c2b25ps');
+    is_deeply(\@list, [ qw/
+        c2b25ps0 c2b25ps1 c2b25ps2 c2b25ps3 c2b25ps4 c2b25ps5 c2b25ps6 c2b25ps7 c2b25ps8 c2b25ps9
+        c2b25psb c2b25psc c2b25psd c2b25pse c2b25psf c2b25psg c2b25psh c2b25psj c2b25psk c2b25psm
+        c2b25psn c2b25psp c2b25psq c2b25psr c2b25pss c2b25pst c2b25psu c2b25psv c2b25psw c2b25psx
+        c2b25psy c2b25psz
+    / ]);
 
 =head1 CONSTANTS
 
